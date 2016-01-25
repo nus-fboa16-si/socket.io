752f454a4dd51f5a098d2132113df4c66b163ed9
Zheng  Weihan <weihan@wscouting.com>
2016-01-25 11:35:15 +0800

    added test-cov task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('test', function(){
   return gulp.src('test/*.js', {read: false})
@@ -12,6 +13,28 @@ gulp.task('test', function(){
       process.exit(1);
     })
     .once('end', function () {
+      process.exit();
+    });
+});
+
+gulp.task('istanbul-pre-test', function () {
+  return gulp.src(['lib/**/*.js'])
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test-cov', ['istanbul-pre-test'], function(){
+  gulp.src(['test/socket.io.js'])
+    .pipe(mocha({
+      reporter: 'dot'
+    }))
+    .pipe(istanbul.writeReports())
+    .once('error', function (){
+      process.exit(1);
+    })
+    .once('end', function (){
       process.exit();
     });
 });
